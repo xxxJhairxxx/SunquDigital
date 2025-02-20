78a2cb7fd7b524734cdbc324fd73eef4bc47e197
jhair <jinfanzonquispe30@gmail.com>
2025-02-20 04:42:36 -0500

    add observer

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
-
-
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-navbar',
-  template: `<nav class=' z-[999]  w-full bg-black py-2 text-[1.6rem] text-white fixed top-0 left-0'>
-    <ul class='flex w-full max-w-[145rem] gap-8 mx-auto items-center '>
-        <li class='mr-auto cursor-pointer'><img src="/assets/images/logo.png"  alt=""></li>
-        @for(item of menu;track $index){
-          <li class="cursor-pointer" >{{ item }}</li>
-        }
-        
+  template: `<nav
+    class=" z-[999]  w-full py-2 text-[1.6rem] text-white fixed top-0 left-0 transition-observer"
+    [class]="isAtTop ? 'bg-transparent' : 'bg-black '"
+  >
+    <ul class="flex w-full max-w-[145rem] gap-8 mx-auto items-center ">
+      <li class="mr-auto cursor-pointer">
+        <img src="/assets/images/logo.png" alt="" />
+      </li>
+      @for(item of menu;track $index){
+      <li class="cursor-pointer">{{ item }}</li>
+      }
     </ul>
-</nav>
-`
+  </nav> <div #trigger class="h-1 absolute"></div>`,
+  styles:`
+  .transition-observer{
+    transition: background .25s ease-in;
+  }
+  `
 })
-export class NavbarComponent {
-   menu : Array<string> = ['Home','Compañia','Portafolio','Blog','Contacto'];
+export class NavbarComponent implements AfterViewInit{
+  menu: Array<string> = ['Home', 'Compañia', 'Portafolio', 'Blog', 'Contacto'];
 
-   
+  isAtTop: boolean = true;
+
+  @ViewChild('trigger', { static: false }) trigger!: ElementRef;
+  observer!: IntersectionObserver;
+
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          this.isAtTop = entry.isIntersecting;
+        });
+      },
+      { threshold: [0] }
+    );
+    this.observer.observe(this.trigger.nativeElement);
+  }
+
+
 }
